fd7a7ccf982ecd54d0f907a2d5470c8a1b7915ef
jacob6838 <jfrye@neaeraconsulting.com>
2023-09-25 11:41:35 -0600

    Re-naming connectionOfTravelAssessmentGroups

--- gui/src/components/map/map-component.tsx
+++ gui/src/components/map/map-component.tsx
@@ -299,7 +299,7 @@ const MapTab = (props: MyProps) => {
     switch (notification.notificationType) {
       case "ConnectionOfTravelNotification":
         const connTravelNotification = notification as ConnectionOfTravelNotification;
-        const connTravelAssessmentGroups = connTravelNotification.assessment.connectionOfTravelAssessment;
+        const connTravelAssessmentGroups = connTravelNotification.assessment.connectionOfTravelAssessmentGroups;
         connTravelAssessmentGroups?.forEach((assessmentGroup) => {
           const ingressLocation: number[] | undefined = connectingLanes.features.find(
             (connectingLaneFeature: MapFeature) => {
