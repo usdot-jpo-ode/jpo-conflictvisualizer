b81bd89e1df147a268c03edd7f0983d74e1146a4
jacob6838 <jfrye@neaeraconsulting.com>
2024-03-29 14:46:31 -0600

    Adding static BSM colors

--- gui/src/components/map/utilities/colors.ts
+++ gui/src/components/map/utilities/colors.ts
@@ -1,12 +1,11 @@
+import { createHash } from "crypto";
 export function generateColorDictionary(inputSet: Set<string>): { [key: string]: string } {
-  const keysArray = Array.from(inputSet);
+  const keysArray = Array.from(inputSet).sort(); // Sort the keysArray
   const colorDictionary: { [key: string]: string } = {};
 
-  const colorStep = 360 / keysArray.length; // Divide the color wheel into equal parts
-
-  keysArray.forEach((key, index) => {
-    const hue = index * colorStep; // Calculate hue for each key
-    const color = `hsl(${hue}, 100%, 35%)`; // Create an HSL color
+  keysArray.forEach((key) => {
+    const hash = createHash("md5").update(key).digest("hex"); // Generate a hash for each key
+    const color = `#${hash.slice(0, 6)}`; // Use the first 6 characters of the hash as the color
     colorDictionary[key] = color;
   });
 
