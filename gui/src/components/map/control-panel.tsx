b0a1e189e860a461f1330611c3cbd205238d53c6
jacob6838 <jfrye@neaeraconsulting.com>
2024-04-30 17:08:37 -0600

    Update control-panel.tsx

--- gui/src/components/map/control-panel.tsx
+++ gui/src/components/map/control-panel.tsx
@@ -24,6 +24,7 @@ import {
   selectBsmEventsByMinute,
   selectBsmEventsByMinute,
   selectBsmTrailLength,
+  selectPlaybackModeActive,
   selectSliderTimeValue,
   setBsmTrailLength,
   setLaneLabelsVisible,
