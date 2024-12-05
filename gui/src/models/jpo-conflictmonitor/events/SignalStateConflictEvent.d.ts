17451c9ab721cc6172d4278d4d5228610d9434a7
john-wiens <jwiens3141@gmail.com>
2024-12-04 17:37:48 -0700

    Updated for Compatibility with new Pedestrian Events

--- gui/src/models/jpo-conflictmonitor/events/SignalStateConflictEvent.d.ts
+++ gui/src/models/jpo-conflictmonitor/events/SignalStateConflictEvent.d.ts
@@ -1,10 +1,18 @@
 /// <reference path="Event.d.ts" />
 type SignalStateConflictEvent = MessageMonitor.Event & {
-  timestamp: number
-  conflictType: J2735MovementPhaseState
-  firstConflictingSignalGroup: number
-  firstConflictingSignalState: J2735MovementPhaseState
-  secondConflictingSignalGroup: number
-  secondConflictingSignalState: J2735MovementPhaseState
-  source: str
+  timestamp: number;
+  conflictType: J2735MovementPhaseState;
+  firstConflictingSignalGroup: number;
+  firstConflictingSignalState: J2735MovementPhaseState;
+  secondConflictingSignalGroup: number;
+  secondConflictingSignalState: J2735MovementPhaseState;
+  source: str;
+  firstIngressLane: number;
+  firstIngressLaneType: str;
+  firstEgressLane: number;
+  firstEgressLaneType: str;
+  secondIngressLane: number;
+  secondIngressLaneType: str;
+  secondEgressLane: number;
+  secondEgressLaneType: str;
 }
